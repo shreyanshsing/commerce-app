887f03e0d0cfaa6d0f327b504202681ff63552f5
shreyanshsingh <shreyanshsinghjee@gmail.com>
2020-07-19 13:10:27 +0530

    Updates

--- precache-manifest.13e8a46a63a4ba6bc33c1258156cbdb7.js
+++ precache-manifest.13e8a46a63a4ba6bc33c1258156cbdb7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb85583ced669858a8ed11c7f8627f84",
+    "revision": "2de3468ccdafb34330ed9a05d062eb5a",
     "url": "/commerce-app/index.html"
   },
   {
-    "revision": "07dd2946b37c7848079e",
-    "url": "/commerce-app/static/css/main.9ede1b7a.chunk.css"
+    "revision": "58f11bfaf59c09cc928f",
+    "url": "/commerce-app/static/css/main.ed238e62.chunk.css"
   },
   {
-    "revision": "4a60b605d36ee63e6d17",
-    "url": "/commerce-app/static/js/2.3c6d94b1.chunk.js"
+    "revision": "0251ea080a8266de2722",
+    "url": "/commerce-app/static/js/2.aa62ae0d.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/commerce-app/static/js/2.3c6d94b1.chunk.js.LICENSE.txt"
+    "url": "/commerce-app/static/js/2.aa62ae0d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "07dd2946b37c7848079e",
-    "url": "/commerce-app/static/js/main.04446406.chunk.js"
+    "revision": "58f11bfaf59c09cc928f",
+    "url": "/commerce-app/static/js/main.e3dcdd8d.chunk.js"
   },
   {
     "revision": "e1f6edce87d365562465",
