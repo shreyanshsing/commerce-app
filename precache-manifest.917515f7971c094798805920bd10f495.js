a5e295fc6d5c1039ffdca20a42f9a3d953384641
shreyanshsingh <shreyanshsinghjee@gmail.com>
2020-07-20 18:53:24 +0530

    Updates

---
+++ precache-manifest.917515f7971c094798805920bd10f495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e685cdcb2d938707f0c3f6dde3ab5c2",
+    "url": "/commerce-app/index.html"
+  },
+  {
+    "revision": "aed328eb661bd87178db",
+    "url": "/commerce-app/static/css/main.b124ac26.chunk.css"
+  },
+  {
+    "revision": "969bfd1e2b1469640c5d",
+    "url": "/commerce-app/static/js/2.b59c12b9.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/commerce-app/static/js/2.b59c12b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed328eb661bd87178db",
+    "url": "/commerce-app/static/js/main.b74559e9.chunk.js"
+  },
+  {
+    "revision": "e1f6edce87d365562465",
+    "url": "/commerce-app/static/js/runtime-main.5f5c4f77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commerce-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
